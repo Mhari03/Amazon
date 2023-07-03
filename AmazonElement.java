c228aa974a9dd09a1a337d7f24f8bc1514b3f6e2
Mhari03 <120275876+Mhari03@users.noreply.github.com>
2023-07-03 20:50:15 +0530

    Add files via upload

---
+++ AmazonElement.java
@@ -0,0 +1,36 @@
+package com.actitime.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class AmazonElement{
+
+	public static void main(String[] args) throws InterruptedException  {
+    
+		
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://amazon.in/");
+		driver.manage().window().maximize();
+		//driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+		//Thread.sleep(4000);
+		driver.findElement(By.xpath("(//input)[4]")).sendKeys("Wrist Watches");
+	    driver.findElement(By.xpath("(//input)[5]")).click();
+	    driver.findElement(By.xpath("//li[@id='p_89/Titan']/../li/span/a/div")).click();
+	    JavascriptExecutor j=(JavascriptExecutor) driver;
+	    j.executeScript("window.scrollBy(0,850)");
+	    driver.findElement(By.xpath("//span[text()='25% Off or more']")).click();
+	    j.executeScript("window.scrollBy(0,850)");
+        driver.findElement(By.xpath("//span[contains(text(),'Analogue')]")).click();
+        j.executeScript("window.scrollBy(0,900)");
+        driver.findElement(By.xpath("//span[@class='a-size-base a-color-base'][normalize-space()='Leather']")).click();
+        Thread.sleep(4000);
+        j.executeScript("window.scrollTo(0,0)");
+        driver.findElement(By.xpath("//span[normalize-space()='2,096']")).click();		
+	}
+
+}
